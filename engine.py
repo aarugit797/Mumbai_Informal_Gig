fc88a5d6ce89fa7182db0c4ebd2c8dab38b881d4
Aaradhya Jain <aaradhyajain1732@gmail.com>
2026-03-10 23:37:14 +0530

    Fix sqlite3 and secure secrets

--- engine.py
+++ engine.py
@@ -7,6 +7,10 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 # 2. Force torch to report that no GPU is available
 torch.cuda.is_available = lambda : False
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
 import streamlit as st
 from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
